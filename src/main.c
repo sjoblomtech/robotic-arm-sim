30c6dbf27bf70714b71c99d5f55f743230fb3130
Tobias Sjöblom <tobias.sjoblom@sjoblomtech.com>
2025-11-01 20:08:42 +0200

    Phase 4A: clearer ASCII visualization with auto-fit view, axes, and labeled joints

--- src/main.c
+++ src/main.c
@@ -7,6 +7,12 @@
     #define M_PI 3.14159265358979323846
 #endif
 
+// Grid constant
+#define GRID_W 41   // Columns
+#define GRID_H 21   // Rows
+#define ORIGIN_X (GRID_W / 2)     // Center horizontally
+#define ORIGIN_Y (GRID_H - 2)     // Near bottom
+
 // Structs
 typedef struct {
     double angle;       // Rotation angle in degrees
@@ -33,14 +39,18 @@ void print_arm(Arm arm);
 double deg2rad(double degrees);
 void jointPositions(const Arm* arm, Vec2* positions);
 void buildArm(Arm* arm);
+void clear_grid(char grid[GRID_H][GRID_W]);
+void draw_axes(char grid[GRID_H][GRID_W]);
+void print_grid(char grid[GRID_H][GRID_W]);
+void world_to_grid(double x, double y, int *gc, int *gr, double scale);
+void draw_line(char grid[GRID_H][GRID_W], int c0, int r0, int c1, int r1, char ch);
+void draw_arm_ascii(const Arm* arm, const Vec2* pos, double scale);
 
 // Main
 int main(void) {
     Arm arm = {0};
 
     buildArm(&arm);
-
-
 
     // Compute and display positions
     Vec2 positions[arm.num_links + 1];
@@ -55,6 +65,8 @@ int main(void) {
     printf("\nEnd effektor position: (%.2f, %.2f)\n", positions[arm.num_links].x,
            positions[arm.num_links].y);
     free(arm.links);
+
+    draw_arm_ascii(&arm, positions, 0.8);
     return 0;
 }
 
@@ -126,3 +138,85 @@ void buildArm(Arm* arm) {
         sprintf(arm->links[i].joint.name, "joint %d", i +1);
     }
 }
+
+// Grid utilities start
+void clear_grid(char grid[GRID_H][GRID_W]) {
+    for (int r = 0; r < GRID_H; r++)
+        for (int c = 0; c < GRID_W; c++)
+            grid[r][c] = ' ';
+}
+
+void draw_axes(char grid[GRID_H][GRID_W]) {
+    // x-axis
+    for (int c = 0; c < GRID_W; c++) grid[ORIGIN_Y][c] = '-';
+    // y-axis
+    for (int r = 0; r < GRID_H; r++) grid[ORIGIN_X][r] = '|';
+    grid[ORIGIN_Y][ORIGIN_X] = '+';     // origin
+}
+
+void print_grid(char grid[GRID_H][GRID_W]) {
+    // top border
+    putchar('\n');
+    for (int c = 0; c < GRID_W + 2; c++) putchar('#');
+    putchar('\n');
+
+    for (int r = 0; r < GRID_H; r++) {
+        putchar('#');
+        for (int c = 0; c < GRID_W; c++) putchar(grid[r][c]);
+        putchar('#');
+        putchar('\n');
+    }
+
+    for (int c = 0; c < GRID_W + 2; c++) putchar('#');
+    putchar('\n');
+}
+// Grid utilities end
+
+// World (double x,y) -> grid (col, row)
+void world_to_grid(double x, double y, int *gc, int *gr, double scale) {
+    // flip y for screen rows 
+    *gc = ORIGIN_X + (int)lround(x * scale);
+    *gr = ORIGIN_Y - (int) lround(y * scale);
+}
+
+void draw_line(char grid[GRID_H][GRID_W], int c0, int r0, int c1, int r1, char ch) {
+    int dc = abs(c1 - c0), dr = -abs(r1 - r0);
+    int sc = (c0 < c1) ? 1 : -1;
+    int sr = (r0 < r1) ? 1 : -1;
+    int err = dc + dr;
+
+    while (1) {
+        if (r0 >= 0 && r0 < GRID_H && c0 >= 0 && c0 < GRID_W)
+            if (grid[r0][c0] == ' ') grid[r0][c0] = ch;     // Don't overwrite joint if already in place
+
+            if (c0 == c1 && r0 == r1) break;
+            int e2 = 2 * err;
+            if (e2 >= dr) { err += dr; c0 += sc; }
+            if (e2 <= dc) { err += dc; r0 += sr; }
+    }
+}
+
+void draw_arm_ascii(const Arm* arm, const Vec2* pos, double scale) {
+    char grid[GRID_H][GRID_W];
+    clear_grid(grid);
+    draw_axes(grid);
+
+    // Plot links as line between joints
+    for (int i = 0; i < arm->num_links; i++) {
+        int c0, r0, c1, r1;
+        world_to_grid(pos[i].x, pos[i].y, &c0, &r0, scale);     // Joint i
+        world_to_grid(pos[i + 1].x, pos[i + 1].y, &c1, &r1, scale); // Joint i + 1
+        draw_line(grid, c0, r0, c1, r1, '.');
+    }
+
+    // Plot joint and end effektor
+    for (int i = 0; i <= arm->num_links; i++) {
+        int c, r; world_to_grid(pos[i].x, pos[i].y, &c, &r, scale);
+        if (r >= 0 && r < GRID_H && c >= 0 && c < GRID_W) {
+            grid[r][c] = (i == 0) ? 'o' : (i == arm->num_links ? 'X' : '*');
+        }
+    }
+
+    print_grid(grid);
+}
+
